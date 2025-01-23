4b016fcac23c374ecc40492791a576155d966c72
Hong29072005 <mrhong563@gmail.com>
2025-01-23 13:21:32 +0700

    Add files via upload

---
+++ test.js
@@ -0,0 +1,8 @@
+ function dark_mode(){
+    document.getElementById("body").style.backgroundColor="  rgb(20, 23, 32)"
+     document.getElementById("body").style.color="white"
+ }
+ function light_mode(){
+    document.getElementById("body").style.backgroundColor=" white"
+     document.getElementById("body").style.color="black "
+ }
